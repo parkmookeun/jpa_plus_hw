f51a3647a97a087c9f23b210bd6337194098134a
parkmookeun <sohappynow12@naver.com>
2024-12-13 17:53:26 +0900

    feat: lv 4. 구현

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.status = 'BLOCKED' WHERE u.id IN :userIds")
+    void updateStatusToBlockedAll(List<Long> userIds);
 }
--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,12 +17,17 @@ public class AdminService {
     // TODO: 4. find or save 예제 개선
     @Transactional
     public void reportUsers(List<Long> userIds) {
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
-
-            user.updateStatusToBlocked();
-
-            userRepository.save(user);
-        }
+//        for (Long userId : userIds) {
+//            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+//
+//            user.updateStatusToBlocked();
+//
+//            userRepository.save(user);
+//        }
+//        List<Long> userIdsList = userRepository.findAllById(userIds)
+//                .stream()
+//                .map(User::getId)
+//                .toList();
+        userRepository.updateStatusToBlockedAll(userIds);
     }
 }
