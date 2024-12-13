e24bb22f63064bc6826f41026c638a1f4a3da710
parkmookeun <sohappynow12@naver.com>
2024-12-13 17:52:52 +0900

    feat: lv 3. 구현

--- src/main/java/com/example/demo/repository/ReservationRepository.java
+++ src/main/java/com/example/demo/repository/ReservationRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
@@ -28,4 +27,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             @Param("startAt") LocalDateTime startAt,
             @Param("endAt") LocalDateTime endAt
     );
+
+    @Query("SELECT r FROM Reservation r JOIN FETCH r.user JOIN FETCH r.item")
+    List<Reservation> findAllInfo();
 }
